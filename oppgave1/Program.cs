86a1c73817deb424b8d7aad82305473b2e247e98
Sigurd Varhaugvik <svar@computas.com>
2023-06-05 22:59:15 +0200

    inital

---
+++ oppgave1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using dotnet_intro.Extensions;
+using oppgave1.Data;
+using oppgave1.Extensions;
+
+var personData = PersonData.HentPersonData();
+
+var personerEldreEnn20MedGyldigEpost = personData.Where(person => person.Alder() > 20 && person.Epost.ErGyldigEpost()).ToList();
+
+personerEldreEnn20MedGyldigEpost.ForEach(person => Console.WriteLine($"{person.Navn} er {person.Alder()} år og har gyldig epost ({person.Epost})."));
